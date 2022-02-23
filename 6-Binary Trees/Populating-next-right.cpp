fd9c9fa7824ae3f1821b07eea62b0d873c8b1bac
Utkarsh Mishra <anindiangeek@outlook.com>
2022-02-23 15:18:29 +0530

    Add problem link and explanation

--- 6-Binary Trees/Populating-next-right.cpp
+++ 6-Binary Trees/Populating-next-right.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 /*
     Problem : Given a binary tree, connect the nodes that are at same level.
+              There is a extra pointer in the structre of tree that is a next node, make that next node point to it's
+   proper place. "We are given a perfect binary tree and we need to populate next pointers in each node of the tree".
+              the is not necessarily perfect is another question.
 
        10                       10 ------> NULL
       / \                     /     \
@@ -11,10 +14,38 @@ using namespace std;
    4   1   2              4 --> 1 -----> 2 -------> NULL
 
     Problem Links :
+    * Note : There are two different questions 1 where given tree is perfect and other where it is not perfect.
     https://www.geeksforgeeks.org/connect-nodes-at-same-level/
-    https://leetcode.com/problems/populating-next-right-pointers-in-each-node
+    https://leetcode.com/problems/populating-next-right-pointers-in-each-node   //  <---  Given Tree is perfect.
+    https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/  // <-- Tree is not perfect.
+
+    Explanation :
+    https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/discuss/962162/C%2B%2B-Iterative-Constant-Space-Solution-Explained-~100-Time-~75-Space
+    https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/discuss/962414/C%2B%2B-Recursion-and-Iteration-with-comments-and-figure-illustration
 */
 
+class SpecialBFS // O(N) && S(1)
+{
+  public:
+    Node *connect(Node *root)
+    {
+        auto head = root;
+        for (; root; root = root->left)
+            // traverse each level - it's just BFS taking advantage of next pointers
+            for (auto cur = root; cur; cur = cur->next)
+                if (cur->left)
+                {
+                    // update next pointers of children if they exist
+                    cur->left->next = cur->right;
+                    if (cur->next)
+                        cur->right->next = cur->next->left;
+                }
+                else
+                    break; // if no children exist, stop iteration
+
+        return head;
+    }
+};
 class BFS // O(N) && S(Width of the tree)
 {
     // in this method we transverse the tree using level order traversal from right to left
@@ -50,7 +81,7 @@ class BFS // O(N) && S(Width of the tree)
     }
 };
 
-class DFS // O(N) && S(logN)
+class DFS // O(N) && S(height of binary tree)
 {
     // in this method we transverse the tree using pre order traversal recursively
     // connecting the links to the child nodes at each level
